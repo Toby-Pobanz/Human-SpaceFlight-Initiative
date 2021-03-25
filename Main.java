ecd1036c16f401da41ffee685b2d7fadf8dc4b35
Student POBANZ <tobiasmpobanz@bangorschools.org>
2021-03-25 12:26:03 -0400

    Updated #1

--- Main.java
+++ Main.java
@@ -1,21 +1,33 @@
 
 import java.sql.*;
 import java.util.*;
-
+import java.nio.file; 
 import java.io.*;
+
 public class Main {
 
     
-    public static void main(String[] args) {
+    public static void main(String[] args, File file) {
     
     String password = "";
     String username = "Admin";
     String DB_URL = "jdbc:derby:AstroDB";
     
-
+    File f = new File("password.dat"); 
     try {
-    if(isFileExists("password.dat")) {
+    if(!f.isFile()) {
         System.out.println("file is true");
+
+        password = new Random().ints(10, 33, 122).collect(StringBuilder::new,
+        StringBuilder::appendCodePoint, StringBuilder::append)
+        .toString();
+        System.out.println(password);
+        File F = new File ("password.dat");
+        FileOutputStream fstream = new FileOutputStream(F);
+        DataOutputStream output = new DataOutputStream(fstream);
+        output.writeUTF(password);
+        output.close();
+
         
     }
     else {
